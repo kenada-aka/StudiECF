5f015f3e58fbcbbc44f303c9554f955cc8a1ae2a
contact@kenada.fr <contact@kenada.fr>
2021-04-12 14:30:25 +0200

    Admin Controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,73 +1,55 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;                         // Permet d'utiliser les routes sans "config/routes.yaml"
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-use App\Repository\UserRepository;
-use App\Entity\User;
-use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 
 use App\Repository\AdminRepository;
 use App\Entity\Admin;
 use App\Form\AdminType;
 
-use Doctrine\ORM\EntityManagerInterface;
-
-
-class DefaultController extends AbstractController
+class AdminController extends AbstractController
 {
-
     private $repository; 
     private $repoTest;
     private $em;
 
-    public function __construct(UserRepository $userRepository, AdminRepository $adminRepository, EntityManagerInterface $em) {
-        $this->repository = $userRepository;
-        $this->repoTest = $adminRepository;
+    public function __construct(AdminRepository $adminRepository, EntityManagerInterface $em)
+    {
+        $this->repository = $adminRepository;
         $this->em = $em;
     }
 
     /**
-     * @Route("/", name="default.property.home")
+     * @Route("/admin/", name="admin.property.home")
      */
 
-    public function home(UserRepository $userRepository)
+    public function home(): Response
     {
-
-    
-        //dump($this->repoTest->findAll());
-
-        return $this->render('index.html.twig', [
-                        'title' => 'CRUD TEST',
-                        'test' => $this->repoTest->findAll()
-                        
-                    ]);
-
+        return $this->render('admin/admin.home.html.twig', [
+            'title' => 'CRUD TEST',
+            'test' => $this->repository->findAll()
+        ]);
     }
-
-     /**
-     * @Route("/addUser", name="default.property.new")
+    
+    /**
+     * @Route("/admin/addUser", name="admin.property.new")
      */
 
     public function addUser(Request $request)
-
     {
-    /*
-        $user = new User();
 
-        $form = $this->createForm(UserType::class, $user);
-
-*/
         $user = new Admin();
 
         $form = $this->createForm(AdminType::class, $user);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if($form->isSubmitted() && $form->isValid())
+        {
 
             $this->em = $this->getDoctrine()->getManager();
 
@@ -86,15 +68,13 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/posts/{id}", name="default.property.edit", methods="GET|POST")
+     * @Route("/admin/posts/{id}", name="admin.property.edit", methods="GET|POST")
      * @param User $user
      */
 
     public function post(User $user, int $id, Request $request)
-
     {
-        //dump($user);
-        //dump($id);
+
         $form = $this->createForm(UserType::class,  $user);
 
         $form->handleRequest($request);
@@ -110,21 +90,18 @@ class DefaultController extends AbstractController
        
 
         return $this->render('test.edit.html.twig', [
-
             'title' => 'TEST CRUD (edit)',
             'form' => $form->createView()
-
         ]);
 
     }
 
     /**
-     * @Route("/posts/{id}", name="default.property.delete", methods="DELETE")
+     * @Route("/admin/posts/{id}", name="admin.property.delete", methods="DELETE")
      * @param User $user
      */
 
     public function delete(User $user, Request $request)
-
     {
         //dump($request);
         if($this->isCsrfTokenValid('delete'. $user->getId(), $request->get('_token')))
@@ -138,11 +115,6 @@ class DefaultController extends AbstractController
         }
         
         return $this->redirectToRoute('admin.property.home');
-        
-        
-
     }
-
-    
 
 }
