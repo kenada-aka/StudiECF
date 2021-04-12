f9d896c21976fff65ab499694af4f461857897fc
contact@kenada.fr <contact@kenada.fr>
2021-04-12 10:18:43 +0200

    Security CSRF

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,34 +7,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;                         // Permet d'utiliser les routes sans "config/routes.yaml"
 
+use App\Repository\UserRepository;
+
+use App\Entity\User;
+//use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class DefaultController extends AbstractController
-
 {
 
     private $repository; 
     private $em;
-/*
+
     public function __construct(UserRepository $userRepository, EntityManagerInterface $em) {
         $this->repository = $userRepository;
         $this->em = $em;
     }
-*/
+
     /**
 
      * @Route("/", name="home")
 
      */
-    public function home()
+    public function home(UserRepository $userRepository)
 
     {
 
-        //UserRepository $userRepository
+    
         
 
         return $this->render('index.html.twig', [
                         'title' => 'Ma page de contact ',
-                        //'test' => $userRepository->findAll()
+                        'test' => $userRepository->findAll()
                         
                     ]);
 
