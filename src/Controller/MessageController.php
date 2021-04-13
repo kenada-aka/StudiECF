8e0bfa07a1fe9860f010f5a7ae39d4f9a7c06f5d
contact@kenada.fr <contact@kenada.fr>
2021-04-13 15:47:14 +0200

    Message draft 2

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -11,18 +11,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 use App\Repository\MessageRepository;
 use App\Entity\Message;
+use App\Form\MessageType;
+
+use App\Repository\UserRepository;
+use App\Entity\User;
 
 class MessageController extends AbstractController
 {
 
-    private $messageRepo; 
+    private $messageRepo;
+    private $userRepo;
     private $em;
 
-    public function __construct(EntityManagerInterface $em, MessageRepository $messageRepository)
+    public function __construct(EntityManagerInterface $em, MessageRepository $messageRepository, UserRepository $userRepository)
     {
         $this->messageRepo = $messageRepository;
+        $this->userRepo = $userRepository;
         $this->em = $em;
     }
 
@@ -34,7 +42,53 @@ class MessageController extends AbstractController
     {
         return $this->render('message/message.home.html.twig', [
             'title' => 'Message / Home',
-            'realties' => $this->messageRepo->findAll()
+            'messages' => $this->messageRepo->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/message/send/{idReceiver}", name="message.send", methods="GET|POST")
+     */
+
+    public function post(int $idReceiver, Request $request)
+    {
+        // Message
+
+        $message = new Message();
+
+        // Sender
+
+        $sender = $this->userRepo->find(1);
+        $message->setIdSender($sender);
+
+        // Receiver
+
+        $receiver = $this->userRepo->find($idReceiver);
+        $message->setIdReceiver($receiver);
+
+
+//date ???
+// type
+        $message->setType(2);
+        $message->setDate(new \DateTime());
+
+        $form = $this->createForm(MessageType::class,  $message);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $this->em->persist($message);
+            $this->em->flush();
+
+            return $this->redirectToRoute('message.home');
+        }
+
+        return $this->render('message/message.send.html.twig', [
+            'title' => 'Message / send',
+            'form' => $form->createView()
+        ]);
+
     }
 }
