772aa237c9b423ea6719ec97939f9bf1c4fa5bb2
contact@kenada.fr <contact@kenada.fr>
2021-04-16 11:17:09 +0200

    Mise en place de quelques fonctionnalités en Javascript

---
+++ public/assets/js/script.js
@@ -0,0 +1,35 @@
+
+// Menu navbar
+
+$("#navbar a").each(function( index ) {
+    if(window.location.href == this.href) this.classList.add("active");
+    else this.classList.remove("active");
+});
+
+if(window.location.pathname == "/")
+{
+    $("#navbar a")[0].classList.add("active");
+}
+
+$("#navbar a").click(function( event ) {
+    $("#navbar a").each(function( index ) {
+        this.classList.remove("active");
+    });
+    this.classList.toggle("active");
+});
+
+// Event carrousel Hero
+
+$("#hero a").click(function( event ) {
+    let parentNode = event.target.parentNode.parentNode;
+    parentNode.children[0].classList.toggle("active");
+    parentNode.children[1].classList.toggle("active");
+    event.preventDefault();
+});
+
+// Event formulaire contact à la page d'accueil
+
+$("#home-contact").submit(function( event ) {
+    alert("Formulaire temporairement désactivé en attendant le nom de domaine...");
+    event.preventDefault();
+});
